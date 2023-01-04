a774de47f69e037a7475e6095f54f690d78ed86a
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-01-03 21:21:09 -0600

    form

--- src/Tasklist.jsx
+++ src/Tasklist.jsx
@@ -1,15 +1,21 @@
-import { tasks } from "./tasks"
+import { tasks as data} from "./tasks";
 import { useState, useEffect } from "react";
 
 
 export default function Tasklist() {
   const [tasks, setTasks] = useState([]);
-  if(tasks.length === 0)
-   return <h1>No hay tareas</h1>
+  useEffect(() => {
+    setTasks(data)
+  }, [])
+  
+  if (tasks.length === 0) return <h1>No hay tareas</h1>;
   return (
     <div>
       {tasks.map((task) => (
-        <div>tarea</div>
+        <div key={task.id}>
+            <h1>{task.title}</h1>
+            <p>{task.description}</p>
+        </div>
       ))}
     </div>
   );
